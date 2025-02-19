0cc7b93382ef6a8c6d33c99cb168e91e9c460ba8
alanoud niaf <alanoudniaf@gmail.com>
2025-02-19 15:06:03 +0300

    Done

--- queries.py
+++ queries.py
@@ -1,27 +1,63 @@
 # pylint: disable=missing-docstring, C0103
 
+
 def directors_count(db):
     # return the number of directors contained in the database
-    pass  # YOUR CODE HERE
+    query ="""
+            SELECT COUNT(*)
+            FROM directors;
+            """
+    db.execute(query)
+    return db.fetchone()[0]
 
 
 def directors_list(db):
     # return the list of all the directors sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query ="""
+            SELECT name
+            FROM directors
+            ORDER BY name;
+            """
+    db.execute(query)
+
+    return [row[0] for row in db.fetchall()]
 
 
 def river_movies(db):
     # return the list of all movies which contain the exact word "river"
     # in their title, sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query = """
+            SELECT title
+            FROM movies
+            WHERE (lower(title) LIKE '% river %' OR
+                   lower(title) LIKE 'river %' OR
+                   lower(title) LIKE '% river' OR
+                   lower(title) = 'river')
+            ORDER BY title;
+            """
+    db.execute(query)
+    return [row[0] for row in db.fetchall()]
 
 
 def directors_named_like_count(db, name):
     # return the number of directors which contain a given word in their name
-    pass  # YOUR CODE HERE
+    query ="""
+            SELECT COUNT(*)
+            FROM directors
+            WHERE name LIKE ?;
+            """
+    db.execute(query,(f"%{name}%",))
+    return db.fetchone()[0]
 
 
 def movies_longer_than(db, min_length):
     # return this list of all movies which are longer than a given duration,
     # sorted in the alphabetical order
-    pass  # YOUR CODE HERE
+    query ="""
+               SELECT title
+               FROM movies
+               WHERE minutes > ?
+               ORDER BY title;
+               """
+    db.execute(query,(min_length,))
+    return [row[0] for row in db.fetchall()]
